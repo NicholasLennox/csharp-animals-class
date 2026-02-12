18026bada97c6b5eb6e3d78a1a19e830228ea7cd
Nicholas Lennox <30431725+NicholasLennox@users.noreply.github.com>
2026-02-12 13:15:37 +0100

    feat(Program.cs): added 'is' keyword to help with kata

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ internal class Program
         pauly.MakeNest();
         pauly.SlideIntoDM();
         pauly.MakeNoise();
-        
+
 
         List<ISwimmable> swimmers = new List<ISwimmable>();
         swimmers.Add(pauly);
@@ -36,6 +36,29 @@ internal class Program
         foreach (var swimmer in swimmers)
         {
             swimmer.Swim();
+
+            if (swimmer is IFlyable flyer) // I am forcing this to show a keyword that is useful for the KATA.
+            {
+                flyer.Fly(); // Swimmers who can also fly (only eagly in this case)
+            }
+
+            // The 'is' keyword can check whether an object implements an interface (or is of a certain type).
+            // 
+            // In this case, we check if the current swimmer also implements IFlyable.
+            // 
+            // The syntax:
+            //     if (swimmer is IFlyable flyer)
+            //
+            // does TWO things:
+            // 1. Checks if swimmer can be treated as IFlyable.
+            // 2. If true, it safely converts (casts) swimmer to IFlyable
+            //    and assigns it to a new variable called "flyer".
+            //
+            // We need the new name ("flyer") because the original variable
+            // is typed as whatever the loop gives us (ISwimmable).
+            // That type does not contain Fly(), so we cannot call Fly() on it directly.
+            //
+            // The new variable has the IFlyable type, which exposes Fly().
         }
 
     }
